08655e1bc4049816f03889dad150899e0f71d437
Adejare <adejare.arasheed@gmail.com>
2024-01-08 21:44:04 +0100

    Models

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""File Storage Type (JSON)"""
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """serializes instances to a JSON file and deserializes
+    JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self) -> dict:
+        """returns the dictionary '__objects'"""
+        return self.__objects
+
+    def new(self, obj) -> None:
+        """sets in '__objects' the "obj" with key "<obj class name>.id"
+
+        Args:
+            obj (dict): dictionary representing an instance
+        """
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self) -> None:
+        """serializes '__objects' to the JSON file (path: __file_path)"""
+        new_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as w_file:
+            json.dump(new_dict, w_file)
+
+    def reload(self) -> None:
+        """deserializes the JSON file to '__objects' (only if the JSON
+        file (__file_path) exist; otherwise, do nothing. If the file
+        doesn't exist, no exception should be raised)"""
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                r_file = json.load(file)
+                for obj, v in r_file.items():
+                    _cls = v['__class__']
+                    del v['__class__']
+                    self.new(eval(_cls)(**v))
+        except FileNotFoundError:
+            pass
