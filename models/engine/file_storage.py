db7f18471bcbba39356b1ef10d315bcb4c357207
Adejare <adejare.arasheed@gmail.com>
2024-01-11 14:32:18 +0100

    Models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """File Storage Type (JSON)"""
 import json
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class FileStorage:
     """serializes instances to a JSON file and deserializes
---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""User Model"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User inherits from the BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
