bae7126c41699ad1b7af96747a6305f6d4cfd55d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-01-12 09:16:01 +0000

    style: pycodestyle fix for filestorage tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -8,6 +8,7 @@ from models.amenity import Amenity
 from models.engine.file_storage import FileStorage
 import json
 
+
 class TestFileStorageMethods(unittest.TestCase):
     """tests all methods of the `FileStorage` class"""
 
@@ -15,16 +16,19 @@ class TestFileStorageMethods(unittest.TestCase):
         """setup for this testcase"""
 
     def test_new(self):
-        """checks that new() updates the __objects dictionary with 
+        """checks that new() updates the __objects dictionary with
         new instances for all classes"""
 
         f1 = BaseModel()
-        self.assertIn(f1.__class__.__name__ + "." + f1.id, models.storage._FileStorage__objects)
+        self.assertIn(f1.__class__.__name__ + "." + f1.id,
+                      models.storage._FileStorage__objects)
         u1 = User()
-        self.assertIn(u1.__class__.__name__ + "." + u1.id, models.storage._FileStorage__objects)
+        self.assertIn(u1.__class__.__name__ + "." + u1.id,
+                      models.storage._FileStorage__objects)
         a1 = Amenity()
-        self.assertIn(a1.__class__.__name__ + "." + a1.id, models.storage.all().keys())
-    
+        self.assertIn(a1.__class__.__name__ + "." + a1.id,
+                      models.storage.all().keys())
+
     def test_all(self):
         """checks that the correct object record is returned"""
         all_objs = models.storage.all()
@@ -40,19 +44,24 @@ class TestFileStorageMethods(unittest.TestCase):
 
         with open("file.json", "r", encoding="utf-8") as file:
             recent_save = json.load(file)
-            self.assertIn("BaseModel" + "." + m2.id , recent_save)
-        
+            self.assertIn("BaseModel" + "." + m2.id, recent_save)
+
     def test_reload(self):
         """checks deserialization process of objects"""
         m3 = BaseModel()
         a3 = Amenity()
         u3 = User()
         models.storage.save()
-        self.assertIn(u3.__class__.__name__ + "." + u3.id, models.storage._FileStorage__objects)
-        self.assertIn(m3.__class__.__name__ + "." + m3.id, models.storage._FileStorage__objects)
-        self.assertIn(a3.__class__.__name__ + "." + a3.id, models.storage.all().keys())
+        self.assertIn(u3.__class__.__name__ + "." + u3.id,
+                      models.storage._FileStorage__objects)
+        self.assertIn(m3.__class__.__name__ + "." + m3.id,
+                      models.storage._FileStorage__objects)
+        self.assertIn(a3.__class__.__name__ + "." + a3.id,
+                      models.storage.all().keys())
         models.storage.reload()
-        self.assertIn(u3.__class__.__name__ + "." + u3.id, models.storage._FileStorage__objects)
-        self.assertIn(m3.__class__.__name__ + "." + m3.id, models.storage._FileStorage__objects)
-        self.assertIn(a3.__class__.__name__ + "." + a3.id, models.storage.all().keys())
-        
+        self.assertIn(u3.__class__.__name__ + "." + u3.id,
+                      models.storage._FileStorage__objects)
+        self.assertIn(m3.__class__.__name__ + "." + m3.id,
+                      models.storage._FileStorage__objects)
+        self.assertIn(a3.__class__.__name__ + "." + a3.id,
+                      models.storage.all().keys())
